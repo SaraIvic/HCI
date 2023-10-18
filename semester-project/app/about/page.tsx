bdf7c3e4eb3a4479ce3705c16640881584063bee
sivicst <saivicst@gmail.com>
2023-10-18 14:29:25 +0200

    First commit

---
+++ semester-project/app/about/page.tsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <main className="flex justify-center text-4x1 p-14">
+      <h1 className="flex justify-center text-4x1 p-14 font-bold">
+        About page
+      </h1>
+    </main>
+  );
+}
+
+export default About;
---
+++ semester-project/app/showcase/page.tsx
@@ -0,0 +1,11 @@
+function Showcase() {
+  return (
+    <main className="flex justify-center text-4x1 p-14">
+      <h1 className="flex justify-center text-4x1 p-14 font-bold">
+        Showcase page
+      </h1>
+    </main>
+  );
+}
+
+export default Showcase;
