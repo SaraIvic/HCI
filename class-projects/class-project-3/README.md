36d5746c0952e8fe37def5d340045dc48cef7c11
sivicst <saivicst@gmail.com>
2023-10-23 15:17:20 +0200

    Add class project 3

---
+++ class-projects/class-project-3/README.md
@@ -0,0 +1,3 @@
+# Class 3 Project
+
+[https://hci-semester-project-4zojacb88-saras-projects-eb55a394.vercel.app/](https://hci-semester-project-4zojacb88-saras-projects-eb55a394.vercel.app/)
--- semester-project/app/adoption/page.tsx
+++ semester-project/app/adoption/page.tsx
@@ -1,11 +1,11 @@
-function About() {
+function Adoption() {
   return (
     <main className="flex justify-center text-4x1 p-14">
       <h1 className="flex justify-center text-4x1 p-14 font-bold">
-        About page
+        Adoption page
       </h1>
     </main>
   );
 }
 
-export default About;
+export default Adoption;
